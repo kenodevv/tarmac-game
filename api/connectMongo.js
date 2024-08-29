e56c1e21b44e2b2758c790be10e5c19ffc39dac6
keno <82222373+kenodevv@users.noreply.github.com>
2024-08-29 21:47:08 +0200

    Update connectMongo.js

--- api/connectMongo.js
+++ api/connectMongo.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const connectDB = async () => {
-  try {
-    console.log('Attempting to connect to MongoDB...');
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('MongoDB connection error:', error.message);
-    console.error('Connection URI:', process.env.MONGO_URI);  // Print the URI (remove in production)
-    process.exit(1); // Exit process with failure
-  }
-};
+    try {
+      console.log('Attempting to connect to MongoDB...');
+      await mongoose.connect(process.env.MONGO_URI);
+      console.log('MongoDB connected successfully');
+    } catch (error) {
+      console.error('MongoDB connection error:', error.message);
+      console.error('Connection URI:', process.env.MONGO_URI);  // Print the URI (remove in production)
+      process.exit(1); // Exit process with failure
+    }
+  };
 
-connectDB();
+module.exports = connectDB
