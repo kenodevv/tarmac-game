21f471320853eb49768574fc134e8cfd3bf5ba7c
keno <82222373+kenodevv@users.noreply.github.com>
2024-08-30 17:16:11 +0200

    Update connectMongo.js

--- connectMongo.js
+++ connectMongo.js
@@ -1,15 +1,19 @@
-const mongoose = require('mongoose')
-
 const connectDB = async () => {
-    try {
-      console.log('Attempting to connect to MongoDB...');
-      await mongoose.connect(process.env.MONGO_URI);
-      console.log('MongoDB connected successfully');
-    } catch (error) {
-      console.error('MongoDB connection error:', error.message);
-      console.error('Connection URI:', process.env.MONGO_URI);  // Print the URI (remove in production)
-      process.exit(1); // Exit process with failure
-    }
-  };
-
-module.exports = connectDB
+  let retries = 5;
+  while (retries) {
+      try {
+          console.log('Attempting to connect to MongoDB...');
+          await mongoose.connect(process.env.MONGO_URI);
+          console.log('MongoDB connected successfully');
+          break;
+      } catch (error) {
+          console.error('MongoDB connection error:', error.message);
+          retries -= 1;
+          console.log(`Retries left: ${retries}`);
+          if (retries === 0) {
+              process.exit(1);
+          }
+          await new Promise(res => setTimeout(res, 5000)); // Warte 5 Sekunden vor dem nächsten Versuch
+      }
+  }
+};
